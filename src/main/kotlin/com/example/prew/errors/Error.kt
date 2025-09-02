56150a9f025fba8326df243a54d34619338f55f3
Georgios Angelopoulos <geoangelotti@users.noreply.github.com>
2025-09-02 21:55:45 +0300

    custom errors

---
+++ src/main/kotlin/com/example/prew/errors/Error.kt
@@ -0,0 +1,10 @@
+package com.example.prew.errors
+
+import com.example.prew.dto.Edge
+
+sealed class Error(override val message: String) : Exception(message) {
+    class AlreadyExistsError(edge: Edge<Int>) : Error("edge from ${edge.from} to ${edge.to} already exists")
+    class NotFoundError(edge: Edge<Int>) : Error("edge from ${edge.from} to ${edge.to} not found")
+    class InvalidEdgeError(edge: Edge<Int>) : Error("cannot create edge from ${edge.from} to ${edge.to}")
+    class DatabaseError(reason: String) : Error("database error: $reason")
+}
