d724006c779b98f18a00a45b26d74930a975bd4c
Georgios Angelopoulos <geoangelotti@users.noreply.github.com>
2025-09-02 21:55:45 +0300

    Add Node Error.kt

--- src/main/kotlin/com/example/prew/errors/Error.kt
+++ src/main/kotlin/com/example/prew/errors/Error.kt
@@ -5,6 +5,7 @@ import com.example.prew.dto.Edge
 sealed class Error(override val message: String) : Exception(message) {
     class AlreadyExistsError(edge: Edge<Int>) : Error("edge from ${edge.from} to ${edge.to} already exists")
     class NotFoundError(edge: Edge<Int>) : Error("edge from ${edge.from} to ${edge.to} not found")
+    class NotFoundNodeError(node: Int) : Error("node $node not found")
     class InvalidEdgeError(edge: Edge<Int>) : Error("cannot create edge from ${edge.from} to ${edge.to}")
     class DatabaseError(reason: String) : Error("database error: $reason")
 }
