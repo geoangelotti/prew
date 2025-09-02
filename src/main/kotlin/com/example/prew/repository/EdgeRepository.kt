fb108c60b2c9987ddc60b02c7aebb175fe3365f5
Georgios Angelopoulos <geoangelotti@users.noreply.github.com>
2025-09-02 21:55:45 +0300

    edge repository

---
+++ src/main/kotlin/com/example/prew/repository/EdgeRepository.kt
@@ -0,0 +1,38 @@
+package com.example.prew.repository
+
+import com.example.prew.dto.Edge
+import com.example.prew.errors.Error
+import com.example.prew.jooq.Tables.EDGES
+import org.jooq.DSLContext
+import org.springframework.stereotype.Repository
+
+@Repository
+class EdgeRepository(private val dsl: DSLContext) {
+    fun insertEdge(edge: Edge<Int>): Result<Unit> {
+        return try {
+            val inserted = dsl.insertInto(EDGES)
+                .set(EDGES.FROM_ID, edge.from)
+                .set(EDGES.TO_ID, edge.to)
+                .onConflictDoNothing()
+                .execute()
+            if (inserted > 0) Result.success(Unit)
+            else Result.failure(Error.AlreadyExistsError(edge))
+            return Result.success(Unit)
+        } catch (e: Exception) {
+            Result.failure(Error.DatabaseError(e.message?: "unknown"))
+        }
+    }
+
+    fun deleteEdge(edge: Edge<Int>): Result<Unit> {
+        return try {
+            val deleted = dsl.deleteFrom(EDGES)
+                .where(EDGES.FROM_ID.eq(edge.from))
+                .and(EDGES.TO_ID.eq(edge.to))
+                .execute()
+            if (deleted > 0) Result.success(Unit)
+            else Result.failure(Error.NotFoundError(edge))
+        } catch (e: Exception) {
+            Result.failure(Error.DatabaseError(e.message ?: "unknown"))
+        }
+    }
+}
