09b5e9345e7b74ea83b5a67f4a2edb6b8cc25222
Georgios Angelopoulos <geoangelotti@users.noreply.github.com>
2025-09-03 10:46:54 +0300

    makes errors dataclasses

--- src/main/kotlin/com/example/prew/errors/Error.kt
+++ src/main/kotlin/com/example/prew/errors/Error.kt
@@ -3,17 +3,18 @@ package com.example.prew.errors
 import com.example.prew.dto.Edge
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import java.time.Instant
 
 data class ErrorResponse(
     val message: String?,
-    val timestamp: String = java.time.Instant.now().toString(),
+    val timestamp: String = Instant.now().toString(),
     val path: String? = null
 )
 
 sealed class Error(override val message: String) : Exception(message) {
     abstract fun toHttpResponse(path: String?): ResponseEntity<ErrorResponse>
 
-    class AlreadyExistsError(edge: Edge<Int>) : Error("edge from ${edge.from} to ${edge.to} already exists") {
+    data class AlreadyExistsError(val edge: Edge<Int>) : Error("edge from ${edge.from} to ${edge.to} already exists") {
         override fun toHttpResponse(path: String?): ResponseEntity<ErrorResponse> {
             return ResponseEntity
                 .status(HttpStatus.CONFLICT)
@@ -21,25 +22,25 @@ sealed class Error(override val message: String) : Exception(message) {
         }
     }
 
-    class NotFoundEdgeError(edge: Edge<Int>) : Error("edge from ${edge.from} to ${edge.to} not found") {
+    data class NotFoundEdgeError(val edge: Edge<Int>) : Error("edge from ${edge.from} to ${edge.to} not found") {
         override fun toHttpResponse(path: String?): ResponseEntity<ErrorResponse> {
             return ResponseEntity.notFound().build()
         }
     }
 
-    class NotFoundNodeError(node: Int) : Error("node $node not found") {
+    data class NotFoundNodeError(val node: Int) : Error("node $node not found") {
         override fun toHttpResponse(path: String?): ResponseEntity<ErrorResponse> {
             return ResponseEntity.notFound().build()
         }
     }
 
-    class InvalidEdgeError(edge: Edge<Int>) : Error("cannot create edge from ${edge.from} to ${edge.to}") {
+    data class InvalidEdgeError(val edge: Edge<Int>) : Error("cannot create edge from ${edge.from} to ${edge.to}") {
         override fun toHttpResponse(path: String?): ResponseEntity<ErrorResponse> {
             return ResponseEntity.badRequest().body(ErrorResponse(message, path = path))
         }
     }
 
-    class DatabaseError(reason: String) : Error("database error: $reason") {
+    data class DatabaseError(val reason: String) : Error("database error: $reason") {
         override fun toHttpResponse(path: String?): ResponseEntity<ErrorResponse> {
             return ResponseEntity.internalServerError().body(ErrorResponse(message, path = path))
         }
